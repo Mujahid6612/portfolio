d9341f634a70ae2b010c6318b2e05a875208d0cd
Mujahid Ali <mujahidali6612@gmail.com>
2025-06-13 12:17:32 +0500

    Add Stats component to display project statistics and integrate it into the homepage

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Main from '../components/Main'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import Experience from '../components/Experience'
+import Stats from '../components/Stats'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <link rel="icon" href="/fav.png" />
       </Head>
       <Main />
+      <Stats />
       <About />
       <Skills />
       <Experience />
